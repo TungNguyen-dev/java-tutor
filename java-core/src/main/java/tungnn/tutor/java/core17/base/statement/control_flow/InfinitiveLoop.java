2222a1c495932d0e633b5117cc1a9f3866b3e46d
tungnn <tungnn.hn@gmail.com>
2024-09-28 18:43:15 +0700

    core17: InfinitiveLoop.java

---
+++ java-core/src/main/java/tungnn/tutor/java/core17/base/statement/control_flow/InfinitiveLoop.java
@@ -0,0 +1,14 @@
+package tungnn.tutor.java.core17.base.statement.control_flow;
+
+public class InfinitiveLoop {
+
+    public static void main(String[] args) {
+        infinitiveFor();
+    }
+
+    static void infinitiveFor() {
+        for (; ; ) {
+            System.out.println("Hello for the base of java core 17");
+        }
+    }
+}

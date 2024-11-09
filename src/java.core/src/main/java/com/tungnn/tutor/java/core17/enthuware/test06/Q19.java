126d97c87d8fdca278db10f4279185e6c85f4089
tungnn <tungnn.hn@gmail.com>
2024-11-09 20:36:13 +0700

    enthuware: test06

---
+++ src/java.core/src/main/java/com/tungnn/tutor/java/core17/enthuware/test06/Q19.java
@@ -0,0 +1,16 @@
+package com.tungnn.tutor.java.core17.enthuware.test06;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Q19 {
+
+    public static void main(String[] args) {
+        Path p1 = Paths.get("photos/../beaches/./catalog/a.txt");
+        Path p2 = p1.normalize();
+        Path p3 = p1.relativize(p2);
+        Path p4 = p2.relativize(p1);
+
+        System.out.println(p1.getNameCount() + " " + p2.getNameCount() + " " + p3.getNameCount() + " " + p4.getNameCount());
+    }
+}
---
+++ src/java.core/src/main/java/com/tungnn/tutor/java/core17/enthuware/test06/Q45.java
@@ -0,0 +1,17 @@
+package com.tungnn.tutor.java.core17.enthuware.test06;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class Q45 {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        Map<String, Object> propMap = props.entrySet()
+            .stream()
+            .collect(Collectors.toMap(e -> e.getKey().toString(), e -> e.getValue().toString()));
+
+        System.out.println(propMap);
+    }
+}

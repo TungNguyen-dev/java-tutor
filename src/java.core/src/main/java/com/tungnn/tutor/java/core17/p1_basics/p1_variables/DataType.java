f64c899e93aecfd0de0894eba4ef096d157ca0c6
tungnn <tungnn.hn@gmail.com>
2024-12-13 23:15:37 +0700

    core17: Primitive wrappers

--- src/java.core/src/main/java/com/tungnn/tutor/java/core17/p1_basics/p1_variables/DataType.java
+++ src/java.core/src/main/java/com/tungnn/tutor/java/core17/p1_basics/p1_variables/DataType.java
@@ -18,25 +18,37 @@ public class DataType {
 
         // Boolean
         boolean bool = true;
-    }
 
-    static class ArrayDataType {
-        char[] c = {'a'};
+        static class CharIntInterchangeable {
+            // int <-- char: Implicit
+            int i = 'c';
 
-        byte[] b = {127};
-        short[] s = {32767};
-        int[] i = {42};
-        long[] l = {42};
 
-        float[] f = {3.14f};
-        double[] d = {3.14};
+            // char <-- int: Implicit in range (0 - 65535 | 0x0000 --> 0xFFFF)
+            char c = 65535;
 
-        boolean[] bool = {true};
-
-        Object[] objects = {new Object()};
+            // char <-- int: Explicit out range
+            char c2 = (char) 1_000_000;
+        }
     }
 
     static class ReferenceDataType {
+        static class ArrayDataType {
+            char[] c = {'a'};
+
+            byte[] b = {127};
+            short[] s = {32767};
+            int[] i = {42};
+            long[] l = {42};
+
+            float[] f = {3.14f};
+            double[] d = {3.14};
+
+            boolean[] bool = {true};
+
+            Object[] objects = {new Object()};
+        }
+
         interface Foo {}
 
         // Object
@@ -46,15 +58,23 @@ public class DataType {
         Foo foo = null;
     }
 
-    static class CharIntInterchangeable {
-        // int <-- char: Implicit
-        int i = 'c';
+    static class PrimitiveWrappers {
+        Character charWrapper = 'c';
+        Byte byteWrapper = 127;
+        Short shortWrapper = 32767;
+        Integer intWrapper = 42;
+        Long longWrapper = 42L;
+        Float floatWrapper = 3.14f;
+        Double doubleWrapper = 3.14;
+        Boolean boolWrapper = true;
 
-
-        // char <-- int: Implicit in range (0 - 65535 | 0x0000 --> 0xFFFF)
-        char c = 65535;
-
-        // char <-- int: Explicit out range
-        char c2 = (char) 1_000_000;
+        char c = charWrapper;
+        byte b = byteWrapper;
+        short s = shortWrapper;
+        int i = intWrapper;
+        long l = longWrapper;
+        float f = floatWrapper;
+        double d = doubleWrapper;
+        boolean bool = boolWrapper;
     }
 }

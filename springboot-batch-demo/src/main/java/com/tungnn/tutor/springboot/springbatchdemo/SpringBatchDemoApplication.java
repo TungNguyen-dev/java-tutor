2c7a54133cf99b0d89b692299e35572b1cc65127
tungnn <tungnn.hn@gmail.com>
2025-05-28 21:21:01 +0700

    springboot: spring-batch-demo init

---
+++ springboot-batch-demo/src/main/java/com/tungnn/tutor/springboot/springbatchdemo/SpringBatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tungnn.tutor.springboot.springbatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchDemoApplication.class, args);
+    }
+
+}

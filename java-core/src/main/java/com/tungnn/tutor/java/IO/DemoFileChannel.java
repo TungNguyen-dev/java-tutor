984d38d187a580402faf9cc9b01e251e80db145e
tungnn <tungnn.hn@gmail.com>
2025-07-26 09:53:21 +0700

    java-io: demo IO operations: stream-byte, stream-char, channel

---
+++ java-core/src/main/java/com/tungnn/tutor/java/IO/DemoFileChannel.java
@@ -0,0 +1,44 @@
+package com.tungnn.tutor.java.IO;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Set;
+
+public class DemoFileChannel {
+
+  public static void main(String[] args) throws IOException {
+    Path path = Path.of("storage", "data", "data-channel.txt");
+    String data = "data for channel write-read";
+    write(path, data);
+    read(path);
+  }
+
+  public static void write(Path dst, Object data) throws IOException {
+    var writeOptionSet = Set.of(StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    try (var channel = FileChannel.open(dst, writeOptionSet)) {
+      ByteBuffer buffer = ByteBuffer.wrap(data.toString().getBytes());
+      int bytesRead;
+      bytesRead = channel.write(buffer);
+      System.out.println(bytesRead);
+    }
+  }
+
+  public static void read(Path src) throws IOException {
+    try (var channel = FileChannel.open(src)) {
+      ByteBuffer buffer = ByteBuffer.allocate(8 * 1024);
+      int bytesRead;
+      do {
+        buffer.clear();
+        bytesRead = channel.read(buffer);
+
+        buffer.flip();
+        while (buffer.hasRemaining()) {
+          System.out.print(buffer.get() + " ");
+        }
+      } while (bytesRead != -1 && bytesRead != 0);
+    }
+  }
+}

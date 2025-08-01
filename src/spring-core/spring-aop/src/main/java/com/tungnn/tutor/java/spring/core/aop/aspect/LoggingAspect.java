7dba1e3e77fbddfa8d8bcce66e265e6bcfc1997b
tungnn <tungnn.hn@gmail.com>
2025-08-01 22:23:02 +0700

    spring-aop: Explicit add JoinPoint

--- src/spring-core/spring-aop/src/main/java/com/tungnn/tutor/java/spring/core/aop/aspect/LoggingAspect.java
+++ src/spring-core/spring-aop/src/main/java/com/tungnn/tutor/java/spring/core/aop/aspect/LoggingAspect.java
@@ -1,6 +1,6 @@
 package com.tungnn.tutor.java.spring.core.aop.aspect;
 
-import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -9,35 +9,53 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
   @Before("execution(* com.tungnn.tutor.java.spring.core.aop.service.MyService.*(..))")
-  public void logBefore() {
-    System.out.println("[@Before] Before method execution");
+  public void logDetailsBefore(JoinPoint joinPoint) {
+    // Gets method name
+    String methodName = joinPoint.getSignature().getName();
+    // Gets method arguments
+    Object[] args = joinPoint.getArgs();
+
+    System.out.println("[@Before] Method: " + methodName + " will execute");
+    if (args.length > 0) {
+      System.out.println("[@Before] Arguments: ");
+      for (Object arg : args) {
+        System.out.println(" - " + arg);
+      }
+    } else {
+      System.out.println("[@Before] No arguments passed");
+    }
   }
 
   @AfterReturning(
       pointcut = "execution(* com.tungnn.tutor.java.spring.core.aop.service.MyService.*(..))",
       returning = "result")
-  public void logReturn(Object result) {
-    System.out.println("[@AfterReturning] Method returned: " + result);
+  public void logDetailsAfterReturning(JoinPoint joinPoint, Object result) {
+    // Gets method name
+    String methodName = joinPoint.getSignature().getName();
+    System.out.println("[@AfterReturning] Method: " + methodName + " executed successfully");
+
+    // Log the return value
+    if (result != null) {
+      System.out.println("[@AfterReturning] Returned value: " + result);
+    } else {
+      System.out.println("[@AfterReturning] Method returned void or null");
+    }
   }
 
   @AfterThrowing(
       pointcut = "execution(* com.tungnn.tutor.java.spring.core.aop.service.MyService.*(..))",
       throwing = "exception")
-  public void logException(Exception exception) {
-    System.out.println("[@AfterThrowing] Exception caught: " + exception.getMessage());
+  public void logDetailsAfterThrowing(JoinPoint joinPoint, Exception exception) {
+    // Gets method name
+    String methodName = joinPoint.getSignature().getName();
+    System.out.println("[@AfterThrowing] Method: " + methodName + " threw an exception");
+    System.out.println("[@AfterThrowing] Exception Message: " + exception.getMessage());
   }
 
   @After("execution(* com.tungnn.tutor.java.spring.core.aop.service.MyService.*(..))")
-  public void logAfter() {
-    System.out.println("[@After] After method execution");
-  }
-
-  @Around("execution(* com.tungnn.tutor.java.spring.core.aop.service.MyService.*(..))")
-  public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-    long start = System.currentTimeMillis();
-    Object proceed = joinPoint.proceed();
-    long executionTime = System.currentTimeMillis() - start;
-    System.out.println("[@Around] Method executed in " + executionTime + "ms");
-    return proceed;
+  public void logDetailsAfter(JoinPoint joinPoint) {
+    // Gets method name
+    String methodName = joinPoint.getSignature().getName();
+    System.out.println("[@After] Method: " + methodName + " has finished execution");
   }
 }

9b98cafde0108938a4834421ccea969b258c0180
tungnn <tungnn.hn@gmail.com>
2026-05-05 23:08:24 +0700

    refactor(selenium): remove hardcoded chromedriver path
    Removed the static block that sets the "webdriver.chrome.driver" system
    property with a hardcoded path. This change improves flexibility and
    allows the driver path to be configured externally, making the utility
    more portable and environment-agnostic.

--- src/java-selenium/src/main/java/tungnn/tutor/java/selenium/util/SeleniumUtil.java
+++ src/java-selenium/src/main/java/tungnn/tutor/java/selenium/util/SeleniumUtil.java
@@ -8,12 +8,6 @@ import java.util.ArrayList;
 
 public final class SeleniumUtil {
 
-  private static final String DRIVER_PATH = "storage/chromedriver-mac-x64/chromedriver";
-
-  static {
-    System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
-  }
-
   private SeleniumUtil() {}
 
   public static WebDriver createChromeDriver() {

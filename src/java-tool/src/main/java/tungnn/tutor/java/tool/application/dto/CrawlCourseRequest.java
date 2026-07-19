85b18af3d19b9f4f501dfd466387305ee62568b0
tungnn <tungnn.hn@gmail.com>
2026-07-19 14:01:07 +0700

    Remove deprecated project java-tool

--- src/java-tool/src/main/java/tungnn/tutor/java/tool/application/dto/CrawlCourseRequest.java
+++
@@ -1,5 +0,0 @@
-package tungnn.tutor.java.tool.application.dto;
-
-import java.nio.file.Path;
-
-public record CrawlCourseRequest(Path coursePath) {}
--- src/java-tool/src/main/java/tungnn/tutor/java/tool/domain/dto/CrawlRequest.java
+++
@@ -1,6 +0,0 @@
-package tungnn.tutor.java.tool.domain.dto;
-
-import java.nio.file.Path;
-import tungnn.tutor.java.tool.shared.CrawlPageEnum;
-
-public record CrawlRequest(CrawlPageEnum page, String url, String unitNo, Path resultDir) {}

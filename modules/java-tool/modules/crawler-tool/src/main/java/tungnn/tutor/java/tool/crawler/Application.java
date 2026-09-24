09ba4d6611c020c3a3dfdcf328904571793707c1
tungnn <tungnn.hn@gmail.com>
2026-09-24 07:01:37 +0700

    fix(crawler): exclude hidden files in traversal
    Fix the crawler directory filter so it exclude hidden files during
    traversal.

--- modules/java-tool/modules/crawler-tool/src/main/java/tungnn/tutor/java/tool/crawler/Application.java
+++ modules/java-tool/modules/crawler-tool/src/main/java/tungnn/tutor/java/tool/crawler/Application.java
@@ -106,7 +106,7 @@ public class Application {
           .filter(
               p -> {
                 try {
-                  return Files.isHidden(p);
+                  return !Files.isHidden(p);
                 } catch (IOException e) {
                   throw new RuntimeException(e);
                 }
